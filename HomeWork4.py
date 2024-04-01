87b3b356ac6da4be7ab6f98025b370f1e3939d05
OlegLogunov <164496716+OlegLogunov@users.noreply.github.com>
2024-04-01 15:04:33 +0300

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,7 @@
+immutable_var = 1, "F", True, "Oleg"
+print("Immutable tuple: ", immutable_var)
+# immutable_var[2] = False  #TypeError: 'tuple' object does not support item assignment Нельзя изменять - заданы конкретные значения
+mutable_list = [29, "Марта", 2024]
+# print(mutable_list)
+mutable_list = ["Вебинар", 1, "Апреля"]
+print("Mutable list: ", mutable_list)
